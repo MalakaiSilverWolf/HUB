06cecf90adbf794443a24b3b7a0b33d276bf57a6
MalakaiSilverWolf <rrr130794@yahoo.com>
2016-06-08 02:12:44 -0500

    Parse Connected
    Connection to parse database is established through heroku, running will
    set an anonymous user, switching between driver and rider will be next
    with integration of Google Maps

--- hubproject/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ hubproject/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -10,20 +10,73 @@ package com.parse.starter;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
 
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+  Switch riderOrDriver;
+
+    public void getStarted(View view){
+        String riderDriver="rider";
+
+        if(riderOrDriver.isChecked()){
+            riderDriver="driver";
+        }
+
+        ParseUser.getCurrentUser().put("riderOrDriver", riderDriver);
+
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Log.i("MyApp", "user signed up");
+                }
+            }
+        });
+    }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+      riderOrDriver = (Switch) findViewById(R.id.riderOrDriverSwitch);
+
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+      getSupportActionBar().hide();
+
+      if(ParseUser.getCurrentUser().getUsername() == null){
+          ParseAnonymousUtils.logIn(new LogInCallback() {
+              @Override
+              public void done(ParseUser user, ParseException e) {
+                  if(e != null){
+                      Log.d("MyApp", "Anonymous login failed.");
+                  }
+                  else{
+                      Log.d("MyApp", "Anonymous user logged in.");
+                  }
+              }
+          });
+      }
+      else{
+          if (ParseUser.getCurrentUser().get("riderOrDriver") != null){
+              Log.i("MyApp","Redirect user");
+          }
+      }
   }
 
   @Override
--- hubproject/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ hubproject/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -30,25 +30,25 @@ public class StarterApplication extends Application {
 
     // Add your initialization code here
     Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-            .applicationId("instagram63537UJDFHEYhdfkefh")
+            .applicationId("liasngawug2424ydfh")
             .clientKey(null)
-            .server("http://instagram319.herokuapp.com/parse/")
+            .server("http://hubproject.herokuapp.com/parse/")
     .build()
     );
 
-      ParseObject gameScore = new ParseObject("GameScore");
-      gameScore.put("score", 1337);
-      gameScore.put("playerName", "Sean Plott");
-      gameScore.put("cheatMode", false);
-      gameScore.saveInBackground(new SaveCallback() {
-          public void done(ParseException e) {
-              if (e == null) {
-                  Log.i("Parse", "Save Succeeded");
-              } else {
-                  Log.i("Parse", "Save Failed");
-              }
-          }
-      });
+      //ParseObject gameScore = new ParseObject("GameScore");
+      //gameScore.put("score", 1337);
+      //gameScore.put("playerName", "Sean Plott");
+      //gameScore.put("cheatMode", false);
+      //gameScore.saveInBackground(new SaveCallback() {
+      //    public void done(ParseException e) {
+      //        if (e == null) {
+      //            Log.i("Parse", "Save Succeeded");
+      //        } else {
+      //            Log.i("Parse", "Save Failed");
+      //        }
+      //    }
+      //});
 
 
       ParseUser.enableAutomaticUser();
